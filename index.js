4b72a2e9fab61d29d51ba575c75bb9b083c54c99
Gleb <gleb.gleb.tula@mail.ru>
2024-05-08 23:23:41 +0300

    fix animation asks

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+
+
 const lift1 = () => {
-    const button = document.querySelector(".lift1");
     const div = document.getElementById("ask1");
     const ava = document.getElementById("ava_lift1");
-    button.addEventListener("click", () => {
-        if (div.classList.contains("ask1_small")){
+    if (div.classList.contains("ask1_small")){
             div.classList.remove("ask1_small");
             div.classList.add("ask1_big")
             const sp = document.createElement('span');
@@ -12,21 +12,18 @@ const lift1 = () => {
             sp.innerHTML = "Digital marketing efforts, instead of handling in-house. They can provide your business with a variety of digital solutions to promote your product or service online and help you hit your marketing goals and grow your business.";
             ava.src = "up.png";
         }
-        else{
+    else{
             div.classList.remove("ask1_big");
             div.classList.add("ask1_small")
             const rem = elementToRemove = document.getElementById("text_ask1");
             div.removeChild(rem);
             ava.src = "down.png";
-        }
-    })
+    }
 }
 
 const lift2 = () => {
-    const button = document.querySelector(".lift2");
     const div = document.getElementById("ask2");
     const ava = document.getElementById("ava_lift2");
-    button.addEventListener("click", () => {
         if (div.classList.contains("ask2_small")){
             div.classList.remove("ask2_small");
             div.classList.add("ask2_big")
@@ -43,14 +40,11 @@ const lift2 = () => {
             div.removeChild(rem);
             ava.src = "down.png";
         }
-    })
 }
 
 const lift3 = () => {
-    const button = document.querySelector(".lift3");
     const div = document.getElementById("ask3");
     const ava = document.getElementById("ava_lift3");
-    button.addEventListener("click", () => {
         if (div.classList.contains("ask3_small")){
             div.classList.remove("ask3_small");
             div.classList.add("ask3_big")
@@ -67,14 +61,11 @@ const lift3 = () => {
             div.removeChild(rem);
             ava.src = "down.png";
         }
-    })
 }
 
 const lift4 = () => {
-    const button = document.querySelector(".lift4");
     const div = document.getElementById("ask4");
     const ava = document.getElementById("ava_lift4");
-    button.addEventListener("click", () => {
         if (div.classList.contains("ask4_small")){
             div.classList.remove("ask4_small");
             div.classList.add("ask4_big")
@@ -91,5 +82,4 @@ const lift4 = () => {
             div.removeChild(rem);
             ava.src = "down.png";
         }
-    })
 }
